3e3fd6341dfc9e77552671fbeac5e0a3f6c0432c
AxelDesert <axel.desert@epitech.eu>
2025-04-05 18:44:16 +0200

    feat(songs): balanced the difficulty and added score and highScore

--- Display.hpp
+++ Display.hpp
@@ -15,42 +15,48 @@
 #include <vector>
 
 class Display {
-    public:
-        enum estadoJuego {
-            INICIO,
-            EN_CURSO,
-            GAME_OVER
-        };
+public:
+    enum estadoJuego {
+        INICIO,
+        EN_CURSO,
+        GAME_OVER
+    };
 
-        Display();
-        sf::Sprite configuarAssetJugador();
-        sf::Sprite configuarPapelPintadoDia();
-        void moverPapelPintado(sf::Clock &tiempo, float &map);
-        void animarPlayer(sf::Clock &tiempo, int &actual);
-        void buclejuego();
-        void secuso(sf::Event event);
-        void actualizarObstaculos();
-        estadoJuego verificarColisionJugador();
-        void mostrarMenuGameOver();
+    Display();
+    sf::Sprite configuarAssetJugador();
+    sf::Sprite configuarPapelPintadoDia();
+    void moverPapelPintado(sf::Clock &tiempo, float &map);
+    void animarPlayer(sf::Clock &tiempo, int &actual);
+    void buclejuego();
+    void secuso(sf::Event event);
+    void actualizarObstaculos();
+    void actualizarVelocidad();
+    estadoJuego verificarColisionJugador();
+    void mostrarMenuGameOver();
 
-    private:
-        float _velocidadY = 0.0f;
-        float _gravedad = 900.0f;
-        float _salto = -350.0f;
-        sf::RenderWindow _ventana;
-        sf::Sprite _jugador;
-        sf::Sprite _papelpintadodia;
-        sf::Sprite _papelpintadodia2;
-        sf::Sprite _papelpintadonoche;
-        sf::Vector2f _postionPlayer;
-        sf::Texture _texturePlayer;
-        sf::Texture _textureBackground;
-        sf::Vector2i _offsetPlayer;
-        float _frameTime;
-        std::vector<Obstaculo> _obstaculos;
-        sf::Texture _textureObstaculo;
-        float _tiempoUltimoObstaculo = 0;
-        estadoJuego _estadoJuego = INICIO;
+private:
+    float _tiempoDeActualizacion;
+    float _santos; // score actuel
+    float _highScore; // meilleur score obtenu
+    float _velocidadBase; // vitesse de base du jeu
+    float _tiempoEntreObstaculos;
+    float _velocidadY = 0.0f;
+    float _gravedad = 900.0f;
+    float _salto = -350.0f;
+    sf::RenderWindow _ventana;
+    sf::Sprite _jugador;
+    sf::Sprite _papelpintadodia;
+    sf::Sprite _papelpintadodia2;
+    sf::Sprite _papelpintadonoche;
+    sf::Vector2f _postionPlayer;
+    sf::Texture _texturePlayer;
+    sf::Texture _textureBackground;
+    sf::Vector2i _offsetPlayer;
+    float _frameTime;
+    std::vector<Obstaculo> _obstaculos;
+    sf::Texture _textureObstaculo;
+    float _tiempoUltimoObstaculo = 0;
+    estadoJuego _estadoJuego = INICIO;
 };
 
 
